963f47ecace94a3c4a053ffb84d1d89e390e2805
natarajan-A1418 <natrajrey0923@gmail.com>
2026-06-22 10:36:09 +0530

    Add save-data API and MySQL integration

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,73 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
+import pymysql
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Sump OHT Automation Project"
+    return "Arjun Flat Water Automation"
 
 @app.route("/api/latest-data")
 def latest_data():
+
+    conn = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="arjun_flat_db"
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT sump_level,
+               oht_level,
+               motor_status
+        FROM water_monitoring
+        ORDER BY id DESC
+        LIMIT 1
+    """)
+
+    row = cursor.fetchone()
+
+    conn.close()
+
     return jsonify({
-        "sump_level": 80,
-        "oht_level": 35,
-        "motor_status": 1
+        "sump_level": row[0],
+        "oht_level": row[1],
+        "motor_status": row[2]
     })
 
-app.run(debug=True)
+@app.route("/api/save-data", methods=["POST"])
+def save_data():
+
+    data = request.json
+
+    sump_level = data["sump_level"]
+    oht_level = data["oht_level"]
+    motor_status = data["motor_status"]
+
+    conn = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="arjun_flat_db"
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO water_monitoring
+        (sump_level, oht_level, motor_status)
+        VALUES (%s, %s, %s)
+    """, (sump_level, oht_level, motor_status))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({
+        "message": "Data Saved Successfully"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
